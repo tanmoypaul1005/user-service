49a5033e713af37b3e1aa72148c148222a5c2a5d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-04 01:18:44 +0600

    done by cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [PrismaModule, ProductModule, UserModule],
+  imports: [PrismaModule, ProductModule, UserModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
