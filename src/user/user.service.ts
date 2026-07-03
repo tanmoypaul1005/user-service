723afce0816fb3ee011e89f5920bb206afad10cc
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-03 12:57:29 +0600

    feat: add bcrypt for password hashing in user creation logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Role } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prisma :PrismaService) {}
+    constructor(private readonly prisma: PrismaService) { }
 
-    async createUser(email: string, password: string , role: Role , name: string , ) {
+    async createUser(email: string, password: string, role: Role, name: string,) {
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        if (await this.prisma.user.findUnique({ where: { email } })) {
+            throw new Error('User with this email already exists');
+        }
+
         return this.prisma.user.create({
             data: {
                 email,
-                password,
+                password:hashedPassword,
                 role,
                 name,
             },
         });
     }
 
-    
+
 }
