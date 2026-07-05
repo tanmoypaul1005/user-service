f7f2cc6a62d769c36f0eb754abba104404ee720f
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-05 21:35:11 +0600

    u[9up

--- src/address/dto/add.address.dto.ts
+++ src/address/dto/add.address.dto.ts
@@ -2,6 +2,7 @@ import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateAddressDto {
+  
   @ApiPropertyOptional({
     description: 'Full address text',
     example: '123 Green Road, Dhanmondi, Dhaka, Bangladesh',
